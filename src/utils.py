40639eaa9f89e63d898526add1af96ad13ee716f
shekharsahu31 <shekharthe07@gmail.com>
2023-10-29 18:03:13 +0530

    Flask App and Predict Pipeline Done

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 
 
 import dill
+#import pickle
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
@@ -56,6 +57,15 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             # bestestimatordict[list(models.keys())[i]] = gs.best_estimator_
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)    
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)    
